1b2abdef0cd65af8a3f027a914f63b78f7cb9476
test02 <test02@getkor.io>
2021-03-24 11:41:27 -0600

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '74f0ff9adf07e2095dd91c633cbdce53'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'efa7307a-bddd-4bba-bba5-3388fd59d13b'
+};
